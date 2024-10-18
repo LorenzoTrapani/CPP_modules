906050faedea918b5fc693abebe4868e84efd18b
lotrapan <lotrapan@gmail.com>
2024-10-18 17:00:49 +0200

    inizio 03

--- cpp03/ex01/ScavTrap.cpp
+++ cpp03/ex01/ScavTrap.cpp
@@ -1,33 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   ScavTrap.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lotrapan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/17 16:34:58 by lotrapan          #+#    #+#             */
-/*   Updated: 2024/10/18 16:57:08 by lotrapan         ###   ########.fr       */
+/*   Created: 2024/10/18 16:59:51 by lotrapan          #+#    #+#             */
+/*   Updated: 2024/10/18 16:59:57 by lotrapan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ClapTrap.hpp"
-
-int main()
-{
-	ClapTrap clapTrap("Pier");
-	ClapTrap clapTrap2(clapTrap);
-	ClapTrap clapTrap3 = clapTrap;
-
-	clapTrap.attack("target");
-	clapTrap.takeDamage(10);
-	clapTrap.beRepaired(5);
-
-	clapTrap2.attack("target2");
-	clapTrap2.takeDamage(5);
-	clapTrap2.beRepaired(2);
-
-	clapTrap3.attack("target3");
-	clapTrap3.takeDamage(2);
-	clapTrap3.beRepaired(1);
-	return 0;
-}
+#include "ScavTrap.hpp"
--- cpp03/ex01/ScavTrap.hpp
+++ cpp03/ex01/ScavTrap.hpp
@@ -1,38 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lotrapan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/17 16:37:18 by lotrapan          #+#    #+#             */
-/*   Updated: 2024/10/18 16:51:48 by lotrapan         ###   ########.fr       */
+/*   Created: 2024/10/18 17:00:08 by lotrapan          #+#    #+#             */
+/*   Updated: 2024/10/18 17:00:34 by lotrapan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CLAPTRAP_HPP
-# define CLAPTRAP_HPP
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
 
 #include <iostream>
 #include <string>
-
-class ClapTrap
-{
-	private:
-	std::string name;
-	int hitPoints;
-	int energyPoints;
-	int attackDamage;
-
-	public:
-	ClapTrap();
-	ClapTrap(std::string const &name);
-	ClapTrap(ClapTrap const &clapTrap);
-	ClapTrap &operator=(ClapTrap const &clapTrap);
-	~ClapTrap();
-	void attack(std::string const &target);
-	void takeDamage(unsigned int amount);
-	void beRepaired(unsigned int amount);
-};
 
 #endif
